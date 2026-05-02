c42337b4e3be5639c7bfc98e08088522314c8481
Delta Student <vk2755735@gmail.com>
2026-05-02 23:49:59 +0530

    update text Full Stack

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Vishal Kumar</h1>
-        <h5 className='text-light'>Fullstack Developer</h5>
+        <h5 className='text-light'>Full Stack Developer</h5>
         <CTA />
         <HeaderSocials />
 
